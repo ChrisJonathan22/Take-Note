0e02137ee266b5bb9d1d7119692d95b65ab2333b
Chris Jonathan <christophereko@hotmail.fr>
2019-02-08 09:02:02 +0000

    I have adjusted the mobile media query from a min-width of 360px to 320px. I've added some space between the form labels and input elements and I've also added a list of default notes to simulate how the list of items from the database will be displayed.

--- take-note/src/components/Home/Home.jsx
+++ take-note/src/components/Home/Home.jsx
@@ -6,17 +6,26 @@ export default class Home extends Component {
     return (
       <div>
         <h1 id="home-title">Welcome to my homepage.</h1>
+        {/* This is the form. */}
         <form id="home-form" action="http://localhost:5000" method="POST">
-            <label>Title</label>
+            <label>Title<span>*</span></label>
             <br/>
-            <input type="text" name="title" id="title" placeholder="Enter a title..."/>
+            <input type="text" name="title" id="title" placeholder="Please enter a title..."/>
             <br/>
-            <label>Info</label>
+            <label>Info<span>*</span></label>
             <br/>
             <textarea name="info" id="info" cols="30" rows="10" placeholder="Please enter some info..."></textarea>
             <br/>
             <input type="submit" value="Add" id="button"/>
         </form>
+        {/* This is where each note will be listed. */}
+        <section id="notes">
+          <ul>
+            <li>Home work</li>
+            <li>Shopping</li>
+            <li>Ring Alice</li>
+          </ul>
+        </section>
       </div>
     )
   }
